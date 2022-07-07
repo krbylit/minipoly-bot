02f8e8d309264fcdf29d6c4aeaafcc549302ac7c
Kirby Little <kirbylittle@gmail.com>
2022-07-07 18:40:26 -0500

    Fixed Player creation issue effecting GO money awarding.

--- minipoly_bot.py
+++ minipoly_bot.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Discord bot implementation of the Minipoly game.
+"""
+
 import discord
 from discord.ext import commands
 import random
-from RealEstateGame import *
+from minipoly import *
+
+__author__ = "Kirby Little"
+__email__ = "krbylit@gmail.com"
 
 client = discord.Client()
 bot = commands.Bot(command_prefix="$")
@@ -35,10 +44,12 @@ rents = [
 
 
 @bot.command(
-    help="Starts a game of Minipoly with 24 spaces and a starting GO space. Rent "
+    help="Starts a game of Minipoly with 24 spaces and a starting GO space. "
+    "Rent "
     "prices for spaces range from $50-$350.",
     brief="Starts a game of Minipoly",
-    name="start",  # optional shorter command call, prefix w/ $ in chat to execute
+    name="start",
+    # optional shorter command call, prefix w/ $ in chat to execute
 )
 async def play_minipoly(ctx):
     """Creates a Minipoly game."""
@@ -51,7 +62,8 @@ async def play_minipoly(ctx):
 @bot.command(
     help="Adds you as a player. You start at GO with $1000.",
     brief="Adds you as a player. You start at GO with $1000.",
-    name="play",  # optional shorter command call, prefix w/ $ in chat to execute
+    name="play",
+    # optional shorter command call, prefix w/ $ in chat to execute
 )
 async def add_player(ctx):
     """Adds the command author as a player."""
@@ -62,21 +74,26 @@ async def add_player(ctx):
 
 
 @bot.command(
-    help="First rolls a die for your movement. Then moves that many spaces for you. "
-    "Movement automatically checks if your new space has an owner, and handles "
+    help="First rolls a die for your movement. Then moves that many spaces "
+    "for you. "
+    "Movement automatically checks if your new space has an owner, "
+    "and handles "
     "your payment of rent to them. Your new current space's info is then "
-    "printed, as well as a notification of rent payment if that happens. Finally "
-    "game status is checked, and a winner is decalred if there is one.",
+    "printed, as well as a notification of rent payment if that "
+    "happens. Finally "
+    "game status is checked, and a winner is declared if there is one.",
     brief="Rolls a die and moves you, paying rent on new space if necessary.",
-    name="roll",  # optional shorter command call, prefix w/ $ in chat to execute
+    name="roll",
+    # optional shorter command call, prefix w/ $ in chat to execute
 )
 async def roll(ctx):
-    """Rolls a die for movement, moves the player and does rent payment handling,
+    """Rolls a die for movement, moves the player and does rent payment
+    handling,
     then prints info on the player's new space occupied."""
     author = ctx.message.author
     user_name = author.name
     die_roll = random.randint(1, 6)
-    money_before_move = game.get_player_account_balance(user_name)
+    # money_before_move = game.get_player_account_balance(user_name)
     game.move_player(user_name, die_roll)
     money_after_move = game.get_player_account_balance(user_name)
     info = game.space_info(user_name)
@@ -99,9 +116,7 @@ async def roll(ctx):
             f"{user_name} was charged ${rent_val} in rent by {owner}!"
         )
     if money_after_move == 0:
-        await ctx.channel.send(
-            f"Ope. {user_name} is out of money :(\nGGWP {user_name}"
-        )
+        await ctx.channel.send(f"Ope. {user_name} is out of money :(\nGGWP {user_name}")
     winner = game.check_game_over()
     if winner != "":
         await ctx.channel.send(
@@ -110,7 +125,8 @@ async def roll(ctx):
 
 
 @bot.command(
-    help="Looks at your current space and prints its information: board position, "
+    help="Looks at your current space and prints its information: board "
+    "position, "
     "owner, rent price, and buy price.",
     brief="Print your current space's info.",
     name="spaceinfo",  # optional shorter command call, prefix w/ $ in chat to
@@ -134,9 +150,10 @@ async def space_info(ctx):
 
 
 @bot.command(
-    help="Prints your information: board position and current account balance.",
-    brief="Prints your information: board position and current account balance.",
-    name="myinfo",  # optional shorter command call, prefix w/ $ in chat to execute
+    help="Prints your information: board position and current account " "balance.",
+    brief="Prints your information: board position and current account " "balance.",
+    name="myinfo",
+    # optional shorter command call, prefix w/ $ in chat to execute
 )
 async def my_info(ctx):
     """Prints player's position and money."""
@@ -151,11 +168,13 @@ async def my_info(ctx):
     help="Buys your current space and transfers ownership to you, if you have "
     "sufficient funds.",
     brief="Buy your current space.",
-    name="buy",  # optional shorter command call, prefix w/ $ in chat to execute
+    name="buy",
+    # optional shorter command call, prefix w/ $ in chat to execute
 )
 async def buy_space(ctx):
     """Buys player's current space."""
-    # NEED TO ADD handling for invalid purchases (not enough money, already owned, etc.)
+    # NEED TO ADD handling for invalid purchases (not enough money, already
+    # owned, etc.)
     author = ctx.message.author
     user_name = author.name
     game.buy_space(user_name)
@@ -165,4 +184,4 @@ async def buy_space(ctx):
     )
 
 
-bot.run("OTgyMTM5NDMxMTQ0MjkyNDAz.G3mZpR.zO8M5bHKv6Yu1alq3v5DyoDf9ICDyn_BndHWj4")
+bot.run("SECRET_KEY")
